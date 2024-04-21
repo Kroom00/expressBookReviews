179ac9022c870b0f9657e96ea629c46273965e05
Kroom00 <88386673+Kroom00@users.noreply.github.com>
2024-04-21 14:24:48 +0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,30 +29,30 @@ public_users.get('/',function (req, res) {
   
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const book = books.find(book => book.isbn === req.params.isbn);
-  if (book) {
-    res.status(200).json(book);
-  } else {
-    res.status(404).json({ message: "Book not found" });
-  }
+public_users.get('/isbn/:isbn', function (req, res) {
+    const book_isbn = books.filter((book) => book.isbn === req.params.isbn);
+    if (book_isbn.length > 0) {
+        res.status(200).json(book_isbn);
+    } else {
+        res.status(404).json({ message: "Book not found" });
+    }
 });
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const booksByAuthor = books.filter(b => b.author === req.params.author);
-    res.status(200).json(booksByAuthor);
+    const booksByAuthor = books.filter(book => book.author === req.params.author);
+    res.send(booksByAuthor);
   });
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const booksByTitle = books.filter(b => b.title === req.params.title);
-    return res.status(200).json(booksByTitle);
+    const booksByTitle = books.filter(book => book.title === req.params.title);
+    res.send(booksByTitle);
   });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-    const book = books.find(b => b.isbn === req.params.isbn);
+    const book = books.find(book => book.isbn === req.params.isbn);
     if (book && book.review) {
       return res.status(200).json({review: book.review});
     } else {
