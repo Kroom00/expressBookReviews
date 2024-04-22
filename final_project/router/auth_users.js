4003d9d772f03d174f79b493f9388e020a5ffea2
Kroom00 <88386673+Kroom00@users.noreply.github.com>
2024-04-22 23:22:00 +0300

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -46,23 +46,36 @@ regd_users.post("/login", (req, res) => {
 
 // Add or modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-    if (req.body.isbn){
-        books[req.body.isbn] = {
-            "author":req.body.author,
-            "title":req.body.title,
-            "reviews":req.body.reviews,
-            }
+    const isbn = req.params.isbn;
+    let book = books[isbn]
+    if (book) { //Check is friend exists
+        let author = req.body.author;
+        let title = req.body.title;
+        let reviews = req.body.reviews;
+
+        if(author) {
+            book["author"] = author
+        }
+        if(title) {
+            book["title"] = title
+        }if(reviews) {
+            book["reviews"] = reviews
+        }
+          
+        books[isbn]=book;
+        res.send(`Book review for ISBN  ${isbn}  has been successfully updated.`);
     }
-    res.send("The review  Has been added!");
-});
+  });
+
+
 
 regd_users.delete("/auth/review/:isbn", (req, res) => {
-
+        const isbn = req.params.isbn;
         const review_ = req.params.review;
         if (review_){
             delete books[review_]
         }
-        res.send(`the review:(${review_})  deleted.`);
+        res.send(`Book review for ISBN  ${isbn}  has been successfully deleted.`);
 });
 
 module.exports.authenticated = regd_users;
